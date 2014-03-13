0219d54bf39173de611477257149cb69dd2d3047
Wayllen <wayllen@163.com>
2014-03-13 11:07:32 +0800

    Create inVokeSqlCmd.ps1

---
+++ inVokeSqlCmd.ps1
@@ -0,0 +1,13 @@
+
+# For use "Invoke-Sqlcmd" command in powershell v1.0, you need to register below two dlls after install sql server 2008R2.
+set-alias installutil $env:windir\microsoft.net\framework\v2.0.50727\installutil
+
+installutil -i “C:\Program Files (x86)\Microsoft SQL Server\100\Tools\Binn\Microsoft.SqlServer.Management.PSProvider.dll”
+
+installutil -i “C:\Program Files (x86)\Microsoft SQL Server\100\Tools\Binn\Microsoft.SqlServer.Management.PSSnapins.dll” 
+
+add-pssnapin sqlserverprovidersnapin100 -ErrorAction SilentlyContinue
+add-pssnapin sqlservercmdletsnapin100 -ErrorAction SilentlyContinue
+
+# Execute sql script on remote server.
+Invoke-Sqlcmd -ServerInstance "remoteServerName"  -InputFile "localSQLScriptfile" -U "DB-username" -P "DB-password"
